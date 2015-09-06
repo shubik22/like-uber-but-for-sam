864938f6b1cfc23fe7d7be0ff46ce68d15e854a3
sam <sam@wealthfront.com>
2015-09-06 00:28:59 -0700

    Add eslint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); // eslint-disable-line new-cap
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); // eslint-disable-line new-cap
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
