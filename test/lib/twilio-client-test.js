16d5018fa8af02aa27d57e9c61e49c3956bf1c76
sam <sam@wealthfront.com>
2015-09-11 00:19:38 -0700

    Add test for twilio-client

---
+++ test/lib/twilio-client-test.js
@@ -0,0 +1,22 @@
+var assert = require('chai').assert;
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+
+describe('twilio-client', function() {
+  var sendMessageStub = sinon.stub();
+
+  var client = proxyquire('../../lib/twilio-client', {
+    twilio: function() {
+      return {
+        sendMessage: sendMessageStub
+      }
+    }
+  });
+
+  describe('#sendMessage', function() {
+    it('should send a messsage to twilio', function() {
+      client.sendMessage('foobar');
+      assert.isTrue(sendMessageStub.calledOnce);
+    });
+  });
+});
