d6d251257d49215aaceadf54759c231697a6c774
sam <sam@wealthfront.com>
2015-09-13 18:56:32 -0700

    Make first test for google-client fetchCoordinates pass

--- test/lib/google-client-test.js
+++ test/lib/google-client-test.js
@@ -1,28 +1,28 @@
 var assert = require('chai').assert;
-var sinon = require('sinon');
 var nock = require('nock');
 
 describe('google-client', function() {
   var googleClient = require('../../lib/google-client');
 
   describe('#fetchCoordinates', function() {
-    it('call the success function with status OK/non-approx location', function() {
-      var successStub = sinon.stub();
+    it('call the success function with status OK/non-approx location', function(done) {
       var address = '541 Cowper Street, Palo Alto, CA';
 
-      nock(process.env.GOOGLE_GEOCODE_URL)
-        .get('/')
-        .query({ key: process.env.GOOGLE_API_KEY, address: address})
-        .replyWithFile(200, __dirname + '../fixtures/google-maps/success.json');
+      var googleEndpoint = nock('https://fake-goog.com')
+        .get('/maps/api/geocode/json')
+        .query(true)
+        .replyWithFile(200, __dirname + '/../../fixtures/google-maps/success.json');
 
       var longAndLat = {
         lat: 37.44793370000001,
         lng: -122.1584026
       };
 
-      googleClient.fetchCoordinates(address);
-
-      assert.isTrue(successStub.withArgs(longAndLat).calledOnce);
+      googleClient.fetchCoordinates(address, function(response) {
+        assert.isTrue(googleEndpoint.isDone());
+        assert.deepEqual(response, longAndLat);
+        done();
+      });
     });
   });
 });
