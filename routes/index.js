696f2f2c873ec5185e7594663af963b4a345d2df
sam <sam@wealthfront.com>
2015-09-13 21:07:46 -0700

    Try hooking it up

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,22 @@
 
 var express = require('express');
 var router = express.Router(); // eslint-disable-line new-cap
+var googleClient = require('../lib/google-client');
+var uberClient = require('../lib/uber-client');
+var twilioClient = require('../lib/twilio-client');
+var parser = require('../lib/message-parser');
+
+function isValidRequest(req) {
+  return req.params.To === process.env.TWILIO_NUMBER &&
+      req.params.From === process.env.SAMS_NUMBER;
+};
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  if (isValidRequest(req)) {
+    twilioClient.sendMessage('It worked! ' + req.params.Body);
+  }
+
   res.render('index', { title: 'Express' });
 });
 
